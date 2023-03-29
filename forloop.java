2bce583a76e32d9c88c2c9cc80b08dc6a4e91411
Saitheja komalla <komallasaiteja@gmail.com>
2023-03-29 17:04:12 +0530

    Create forloop.java

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class forloop{
+  public static void main(String args[]){
+    int n=17,i,sum;
+    for(i=1;i<10;i++){
+      sum=n*i;
+      System.out.println(n+" * "+i+" = "+sum);
+    }
+  }
+}
+      
