4545b41302febc25d5a13cb00365ec2e011cd72c
Saitheja <komallasaiteja@gmail.com>
2023-03-29 17:10:17 +0530

    new branch with scanner class

--- forloop.java
+++ forloop.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class forloop{
   public static void main(String args[]){
-    int n=17,i,sum;
+    int n,i,sum;
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter which table you want to print");
+    n=sc.nextInt();
     for(i=1;i<10;i++){
       sum=n*i;
       System.out.println(n+" * "+i+" = "+sum);
